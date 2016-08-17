5e51be0b718c371a9884c5a7863a8dcb06ee083c
Kuu Miyazaki <miyazaqui@gmail.com>
2016-08-17 19:44:24 +0900

    Use another embed code

--- const.js
+++ const.js
@@ -1,5 +1,5 @@
 module.exports = {
   PCODE: 'ZxNGgyOhy-q1LotjzCC58NUpXlWV',
   PLAYER_ID: 'b3eba00b92534defb7558feaeb504c89',
-  EMBED_CODE: 'AzNzBkNTE69eVpdYMwFX_eV_cyrApr-3'
+  EMBED_CODE: 'Npa3FkNTE6Em9O6_FhMn4JGwyDgBhWqr'
 };
